088fc57c2fa54623c21f82a4bf77987c18eeff35
Alexey Grachov <grachov.alexey@gmail.com>
2019-09-07 18:13:55 +0300

    Switch args to env.

--- main.go
+++ main.go
@@ -9,13 +9,16 @@ import (
 )
 
 func main() {
-	if len(os.Args) < 3 {
-		log.Fatal("too few arguments")
-	}
 	var (
-		token = os.Args[1]
-		chat  = os.Args[2]
+		token = os.Getenv("INPUT_TOKEN")
+		chat  = os.Getenv("INPUT_CHAT")
 	)
+	if token == "" {
+		log.Fatal("token input is required")
+	}
+	if chat == "" {
+		log.Fatal("chat input is required")
+	}
 	c := tbot.NewClient(token, http.DefaultClient, "https://api.telegram.org")
 	_, err := c.SendMessage(chat, "test")
 	if err != nil {

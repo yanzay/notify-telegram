332a994c72742f2df5375510debe24eea6e4f07a
Alexey Grachov <grachov.alexey@gmail.com>
2019-09-07 18:30:02 +0300

    Dump environment.

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 	"os"
@@ -20,6 +21,16 @@ func main() {
 		log.Fatal("chat input is required")
 	}
 	c := tbot.NewClient(token, http.DefaultClient, "https://api.telegram.org")
+	env := []string{
+		"GITHUB_REF", "GITHUB_SHA", "GITHUB_REPOSITORY",
+		"GITHUB_ACTOR", "GITHUB_WORKFLOW", "GITHUB_HEAD_REF",
+		"GITHUB_BASE_REF", "GITHUB_EVENT_NAME", "GITHUB_WORKSPACE",
+		"GITHUB_ACTION", "GITHUB_EVENT_PATH", "RUNNER_OS",
+		"RUNNER_TOOL_CACHE", "RUNNER_TEMP", "RUNNER_WORKSPACE",
+	}
+	for _, v := range env {
+		fmt.Printf("%s: %s\n", v, os.Getenv(v))
+	}
 	_, err := c.SendMessage(chat, "test")
 	if err != nil {
 		log.Fatalf("unable to send message: %v", err)

6f7bde72b17602f7623d1825b49b5a61af984ed4
Alexey Grachov <grachov.alexey@gmail.com>
2019-09-07 18:54:13 +0300

    Add info to message.

--- main.go
+++ main.go
@@ -11,8 +11,9 @@ import (
 
 func main() {
 	var (
-		token = os.Getenv("INPUT_TOKEN")
-		chat  = os.Getenv("INPUT_CHAT")
+		token  = os.Getenv("INPUT_TOKEN")
+		chat   = os.Getenv("INPUT_CHAT")
+		status = os.Getenv("INPUT_STATUS")
 	)
 	if token == "" {
 		log.Fatal("token input is required")
@@ -20,19 +21,16 @@ func main() {
 	if chat == "" {
 		log.Fatal("chat input is required")
 	}
+	if status == "" {
+		log.Fatal("status input is required")
+	}
 	c := tbot.NewClient(token, http.DefaultClient, "https://api.telegram.org")
-	env := []string{
-		"GITHUB_REF", "GITHUB_SHA", "GITHUB_REPOSITORY",
-		"GITHUB_ACTOR", "GITHUB_WORKFLOW", "GITHUB_HEAD_REF",
-		"GITHUB_BASE_REF", "GITHUB_EVENT_NAME", "GITHUB_WORKSPACE",
-		"GITHUB_ACTION", "GITHUB_EVENT_PATH", "RUNNER_OS",
-		"RUNNER_TOOL_CACHE", "RUNNER_TEMP", "RUNNER_WORKSPACE",
-		"STEPS_CONTEXT", "JOB_CONTEXT",
-	}
-	for _, v := range env {
-		fmt.Printf("%s: %s\n", v, os.Getenv(v))
-	}
-	_, err := c.SendMessage(chat, "test")
+
+	workflow := os.Getenv("GITHUB_WORKFLOW")
+	repo := os.Getenv("GITHUB_REPOSITORY")
+	msg := fmt.Sprintf(`*%s*: %s (%s)`, status, repo, workflow)
+
+	_, err := c.SendMessage(chat, msg, tbot.OptParseModeMarkdown)
 	if err != nil {
 		log.Fatalf("unable to send message: %v", err)
 	}

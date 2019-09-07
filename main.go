1db7b992e90077be9ef8e84c8c95128566cde622
Alexey Grachov <grachov.alexey@gmail.com>
2019-09-07 19:24:42 +0300

    Use default status in action.

--- main.go
+++ main.go
@@ -10,18 +10,25 @@ import (
 	"github.com/yanzay/tbot/v2"
 )
 
-var icons = map[string]string{
-	"failure":   "❗",
-	"cancelled": "❕",
-	"success":   "✅",
-}
-
 func main() {
+	icons := map[string]string{
+		"failure":   "❗",
+		"cancelled": "❕",
+		"success":   "✅",
+	}
+
 	var (
+		// inputs
 		token  = os.Getenv("INPUT_TOKEN")
 		chat   = os.Getenv("INPUT_CHAT")
 		status = os.Getenv("INPUT_STATUS")
+
+		// github env
+		workflow = os.Getenv("GITHUB_WORKFLOW")
+		repo     = os.Getenv("GITHUB_REPOSITORY")
+		commit   = os.Getenv("GITHUB_SHA")
 	)
+
 	if token == "" {
 		log.Fatal("token input is required")
 	}
@@ -31,15 +38,12 @@ func main() {
 	if status == "" {
 		log.Fatal("status input is required")
 	}
+
 	c := tbot.NewClient(token, http.DefaultClient, "https://api.telegram.org")
 
 	icon := icons[strings.ToLower(status)]
-
-	workflow := os.Getenv("GITHUB_WORKFLOW")
-	repo := os.Getenv("GITHUB_REPOSITORY")
-	commit := os.Getenv("GITHUB_SHA")
 	link := fmt.Sprintf("https://github.com/%s/commit/%s/checks", repo, commit)
-	msg := fmt.Sprintf(`%s*%s*: %s [%s](%s)`, icon, status, repo, workflow, link)
+	msg := fmt.Sprintf(`%s*%s*: %s ([%s](%s))`, icon, status, repo, workflow, link)
 
 	_, err := c.SendMessage(chat, msg, tbot.OptParseModeMarkdown)
 	if err != nil {
